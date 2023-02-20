79ad0f1bae773aa64213e6ac06e1db2457032259
iFargle <albert@sysctl.io>
2023-02-20 14:59:45 +0900

    test

--- server.py
+++ server.py
@@ -45,6 +45,8 @@ if AUTH_TYPE.lower() == "oidc":
         }
     }
     """
+    LOG.debug("Secrets")
+    LOG.debug(client_secrets)
     with open("/app/instance/secrets.json", "w+") as secrets_json:
         secrets_json.write(client_secrets)
     
@@ -56,7 +58,7 @@ if AUTH_TYPE.lower() == "oidc":
         'OIDC_ID_TOKEN_COOKIE_SECURE': False,
         'OIDC_REQUIRE_VERIFIED_EMAIL': False,
         'OIDC_USER_INFO_ENABLED': True,
-        'OIDC_OPENID_REALM': 'flask-demo',
+        'OIDC_OPENID_REALM': 'Headscale-WebUI',
         'OIDC_SCOPES': ['openid', 'email', 'profile'],
         'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
     })
