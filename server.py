77f7b8138ec49d7715ca9701b7ecbf01e1cd3110
iFargle <albert@sysctl.io>
2023-03-17 14:40:39 +0900

    test

--- server.py
+++ server.py
@@ -433,9 +433,9 @@ def add_user():
     user_name      = escape(json_response['name'])
     url            = headscale.get_url()
     api_key        = headscale.get_api_key()
-    json_data      = json.loads(str('{"name":"'+user_name+'"}'))
+    payload        = '{"name":"'+user_name+'"}'
 
-    return headscale.add_user(url, api_key, json_data)
+    return headscale.add_user(url, api_key, payload)
 
 @app.route('/api/delete_user', methods=['POST'])
 @oidc.require_login
