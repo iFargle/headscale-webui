869817a5016c95af6c97bb29d99ff9ff4a160c95
iFargle <albert@sysctl.io>
2023-03-21 19:40:36 +0900

    test

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,41 +5,31 @@ $(document).ready(function() {
     var trig = 1;
     //fix for chrome
     $("#search").addClass('searchbarfix');
-    
-    
       //animate searchbar width increase to  +150%
       $('#search').click(function(e) {
            //handle other nav elements visibility here to avoid push down 
         $('.search-hide').addClass('hide');
        if (trig == 1){
           $('#navfix2').animate({
-            width: '+=150',
+            width: '+=210',
             marginRight: 0
-          }, 400);
+          }, 200);
           
          trig ++; 
          }
-    
       });
     
       // if user leaves the form the width will go back to original state
-    
       $("#search").focusout(function() {
-      
           $('#navfix2').animate({
-          width: '-=150'
-        }, 400);
+          width: '-=210'
+        }, 200);
        trig = trig - 1;
         //handle other nav elements visibility first to avoid push down
             $('.search-hide').removeClass('hide');
-    
-    
-    
       });
-    
     });
     
-
 //-----------------------------------------------------------
 // General Helpers
 //-----------------------------------------------------------
