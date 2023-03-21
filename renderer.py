47ede1fb9c3b3b94f4850b9e5b27e5fc5e711947
iFargle <albert@sysctl.io>
2023-03-21 15:19:03 +0900

    test

--- renderer.py
+++ renderer.py
@@ -586,7 +586,7 @@ def render_search():
         <li>    
             <div class="input-field">
                 <input id="search" type="search" required>
-                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <label class="label-icon right" for="search"><i class="material-icons">search</i></label>
                 <i class="material-icons">close</i>
             </div>
         </li>  
