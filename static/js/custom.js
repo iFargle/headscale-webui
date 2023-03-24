b5a08a78d4dd348b0a30f5c24e4ea4edd7faa29c
iFargle <albert@sysctl.io>
2023-03-24 12:44:34 +0900

    test

--- static/js/custom.js
+++ static/js/custom.js
@@ -810,11 +810,11 @@ function toggle_route(route_id, current_state, page) {
     var enabledClass  = ""
 
     if (page == "routes") {
-        disabledClass   = "waves-effect waves-light btn-small red-text   lighten-2 tooltipped";
+        disabledClass   = "waves-effect waves-light btn-small red-text lighten-2 tooltipped";
         enabledClass    = "waves-effect waves-light btn-small green-text lighten-2 tooltipped";
     }
     if (page == "machines") {
-        disabledClass   = "material-icons red-text   text-lighten-2 tooltipped";
+        disabledClass   = "material-icons red-text text-lighten-2 tooltipped";
         enabledClass    = "material-icons green-text text-lighten-2 tooltipped";
     }
     var disabledTooltip = "Click to enable"
