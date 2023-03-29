d4e17f2506d25c35b5b2404453a076481585e62b
iFargle <albert@sysctl.io>
2023-03-29 14:58:57 +0900

    test

--- renderer.py
+++ renderer.py
@@ -827,24 +827,28 @@ def render_routes():
                 # and which route ID to make primary.  We then toggle each route
                 # in order, with the primary route being toggled last
                 # Step 1:  Create an array of all route_id's
-                payload = "{"
-                index = 0
+                # payload = "{"
+                # index = 0
 
                 # Is there a better way to do this??
-                for item in route_id_list: 
-                    payload += "\""+str(index)+"\": \""+str(item)+"\""
-                    index += 1
-                    if index != len(route_id_list): payload += ", "
-                payload+="}"
-                app.logger.debug("JSON:  %s", str(payload))
-                json_payload = json.loads(payload)
+                # for item in route_id_list: 
+                #     payload += "\""+str(index)+"\": \""+str(item)+"\""
+                #     index += 1
+                #     if index != len(route_id_list): payload += ", "
+                # payload+="}"
+                # app.logger.debug("JSON:  %s", str(payload))
+                # json_payload = json.loads(payload)
 
+                payload = []
+                # Is there a better way to do this??
+                for item in route_id_list: payload.append(int(item))
+                 
                 app.logger.debug("[%s] Machine:  [%s]  %s : %s / %s", str(route_id), str(machine_id), str(machine), str(is_enabled), str(is_primary))
                 app.logger.debug(str(all_routes["routes"][idx]))
 
                 # Set up the display code:
-                enabled_display_enabled  = "<i id='"+str(route_id)+"' onclick='toggle_failover_route("+str(route_id)+", \"True\", \"routes\" , "+str(route_prefix)+", "+str(json_payload)+")'  class='material-icons green-text text-lighten-2 tooltipped' data-tooltip='Click to disable'>fiber_manual_record</i>"
-                enabled_display_disabled = "<i id='"+str(route_id)+"' onclick='toggle_failover_route("+str(route_id)+", \"False\", \"routes\", "+str(route_prefix)+", "+str(json_payload)+")' class='material-icons red-text text-lighten-2 tooltipped' data-tooltip='Click to enable'>fiber_manual_record</i>"
+                enabled_display_enabled  = "<i id='"+str(route_id)+"' onclick='toggle_failover_route("+str(route_id)+", \"True\", \"routes\" , "+str(route_prefix)+", "+str(payload)+")'  class='material-icons green-text text-lighten-2 tooltipped' data-tooltip='Click to disable'>fiber_manual_record</i>"
+                enabled_display_disabled = "<i id='"+str(route_id)+"' onclick='toggle_failover_route("+str(route_id)+", \"False\", \"routes\", "+str(route_prefix)+", "+str(payload)+")' class='material-icons red-text text-lighten-2 tooltipped' data-tooltip='Click to enable'>fiber_manual_record</i>"
                 primary_display_enabled  = "<i id='"+str(route_id)+"-primary' class='material-icons green-text text-lighten-2'>fiber_manual_record</i>"
                 primary_display_disabled = "<i id='"+str(route_id)+"-primary' class='material-icons red-text text-lighten-2'>fiber_manual_record</i>"
                 
