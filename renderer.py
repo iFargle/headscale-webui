d801f7bbeb380eed17d54767ae292c144ded8499
iFargle <albert@sysctl.io>
2023-03-29 12:20:18 +0900

    test

--- renderer.py
+++ renderer.py
@@ -835,10 +835,10 @@ def render_routes():
                 app.logger.debug(str(all_routes["routes"][idx]))
 
                 # Set up the display code:
-                enabled_display_enabled  = "<i id='"+route["id"]+"' onclick='toggle_route("+route["id"]+", \"True\", \"routes\")'  class='material-icons green-text text-lighten-2 tooltipped' data-tooltip='Click to disable'>fiber_manual_record</i>"
-                enabled_display_disabled = "<i id='"+route["id"]+"' onclick='toggle_route("+route["id"]+", \"False\", \"routes\")' class='material-icons red-text text-lighten-2 tooltipped' data-tooltip='Click to enable' >fiber_manual_record</i>"
+                enabled_display_enabled  = "<i id='"+str(route_id)+"' onclick='toggle_route("+int(route_id)+", \"True\", \"routes\")'  class='material-icons green-text text-lighten-2 tooltipped' data-tooltip='Click to disable'>fiber_manual_record</i>"
+                enabled_display_disabled = "<i id='"+str(route_id)+"' onclick='toggle_route("+int(route_id)+", \"False\", \"routes\")' class='material-icons red-text text-lighten-2 tooltipped' data-tooltip='Click to enable'>fiber_manual_record</i>"
                 primary_display_enabled  = "<i id='"+str(route_id)+"-primary' class='material-icons green-text text-lighten-2'>fiber_manual_record</i>"
-                primary_display_disabled = "<i id='"+str(route_id)+"-primary' class='material-icons red-text text-lighten-1'>fiber_manual_record</i>"
+                primary_display_disabled = "<i id='"+str(route_id)+"-primary' class='material-icons red-text text-lighten-2'>fiber_manual_record</i>"
                 
                 # Set displays:
                 enabled_display = enabled_display_enabled if is_enabled else enabled_display_disabled
