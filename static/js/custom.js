1f6419b68f21c694d6cb0b5aae234634d60c7edd
iFargle <albert@sysctl.io>
2023-03-30 12:03:55 +0900

    test

--- static/js/custom.js
+++ static/js/custom.js
@@ -919,10 +919,10 @@ function toggle_failover_route_routespage(routeid, current_state, prefix, route_
                 var primary_element = document.getElementById(routeid+"-primary")
                 var primary_status = routes["routes"][route_id_list[i]]["isPrimary"]
                 console.log("Setting primary class '"+routeid+"-primary':  "+primary_status)
-                if (primary_status == "true") {
+                if (primary_status == true) {
                     console.log("Detected this route is primary.  Setting the class")
                     primary_element.className = enabledClass
-                } else if (primary_status == "false") {
+                } else if (primary_status == false) {
                     console.log("Detected this route is NOT primary.  Setting the class")
                     primary_element.className = disabledClass
                 }
